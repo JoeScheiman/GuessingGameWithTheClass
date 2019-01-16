2bfe81a5e6814544d2dbb71ec61f489ca4265196
Joe Scheiman <scheiman.joe@gmail.com>
2019-01-16 11:26:37 -0500

    Added goodbye

--- GuessingGameWithClassmates/Program.cs
+++ GuessingGameWithClassmates/Program.cs
@@ -7,6 +7,7 @@ namespace GuessingGameWithClassmates
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye!");
         }
     }
 }
